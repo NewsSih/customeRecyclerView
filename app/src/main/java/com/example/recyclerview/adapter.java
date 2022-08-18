69a5d0a0818582230510c912e4d8f71000213f0f
admin <nessih8@gmail.com>
2022-08-18 12:17:39 +0530

    Initial commit

---
+++ app/src/main/java/com/example/recyclerview/adapter.java
@@ -0,0 +1,13 @@
+package com.example.recyclerview;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class adapter extends RecyclerView.ViewHolder {
+    public adapter(@NonNull View itemView) {
+
+        super(itemView);
+    }
+}
